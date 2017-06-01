9f98dcaf7e4cd2c88fc68e7dca55c10afd199dcf
NineFourFour <james.hickey@webelevate.ie>
2017-06-01 14:49:56 +0100

    Update

--- ExerciseSolution/src/sheet12CustomerPizzaArray/Test.java
+++ ExerciseSolution/src/sheet12CustomerPizzaArray/Test.java
@@ -11,17 +11,23 @@ public class Test {
 		String[] topping6 = {"Garlic","Parmesan", "Bacon", "Mushrooms","Spinch"};
 		
 		Pizza onePizza = new Pizza(topping1, PizzaSize.SIZE_7);
+		//System.out.println("onePizza Cost: "+onePizza.calculatePrice());
 		Pizza twoPizza = new Pizza(topping2, PizzaSize.SIZE_9);
+		//System.out.println("twoPizza Cost: "+twoPizza.calculatePrice());
 		Pizza threePizza = new Pizza(topping3, PizzaSize.SIZE_15);
+		/*System.out.println("threePizza Cost: "+threePizza.calculatePrice());
+		System.out.println("Total Pizza Cost: "+(onePizza.calculatePrice()+twoPizza.calculatePrice()+threePizza.calculatePrice()));*/
 		Pizza quesadillas12 = new Pizza(topping4, PizzaSize.SIZE_12);
 		Pizza meatLovers15 = new Pizza(topping5, PizzaSize.SIZE_15);
 		Pizza baconAlfredo12 = new Pizza(topping6, PizzaSize.SIZE_12);
 		//onePizza.setDiameter(24);
-		
+		/*First customers*/
 		Pizza[] arrayOfPizza = {onePizza, twoPizza, threePizza};
 		Customer newCustomer = new Customer("Hannibal Lecter","23 new Avenue Boston", "555-345-6789",arrayOfPizza);
 		System.out.println(newCustomer.toString());
 		System.out.println();
+		
+		/*second customer*/
 		Pizza[] pizzaTwoArray = {quesadillas12,meatLovers15,baconAlfredo12};
 		Customer secondCustomer = new Customer();
 		secondCustomer.setName("Dexter Morgan");
